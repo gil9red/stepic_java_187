6b80ba869d8be11caf3edbe919f51d0643b8528d
Ilya Petrash <ilya.petrash@inbox.ru>
2016-09-06 01:17:24 +0400

    Added few examples

---
+++ 12756_step_8/src/Quiz.java
@@ -0,0 +1,15 @@
+/**
+ * Created by ipetrash on 05.09.2016.
+ */
+
+import java.security.MessageDigest;
+
+public class Quiz {
+    public static void main(String[] args) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] digest = md.digest("abracadabra".getBytes("UTF-8"));
+        for (byte b : digest) {
+            System.out.printf("%02x", b);
+        }
+    }
+}
---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+/**
+ * HelloWorld class.
+ *
+ */
+public class HelloWorld {
+    /**
+     *
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Arguments: " + Arrays.asList(args));
+    }
+}
+
+
