d34d8f2b42ea9b6db861a5af58d97b2fcd1905a5
Ilya Petrash <ilya.petrash@inbox.ru>
2016-09-10 23:51:37 +0400

    Add 12760_step_3

---
+++ 12760_step_3/src/main.java
@@ -0,0 +1,31 @@
+/**
+ * Created by ipetrash on 10.09.2016.
+ */
+public class main {
+    /*
+    Реализуйте метод, который возвращает букву, стоящую в таблице UNICODE после символа "\" (обратный слэш) \
+    на расстоянии a.
+
+    В качестве примера написано заведомо неправильное выражение. Исправьте его.
+    Sample Input 1:
+        12
+
+    Sample Output 1:
+        h
+
+    Sample Input 2:
+        29
+
+    Sample Output 2:
+        y
+    */
+    public static char charExpression(int a) {
+        return (char) ('\\' + a);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(charExpression(12));
+        System.out.println(charExpression(29));
+    }
+
+}
